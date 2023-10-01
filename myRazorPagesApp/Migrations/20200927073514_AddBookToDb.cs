7c78315698f3c6bdf42896e306a58b520f5eb3c0
Parupa11i <varun.parupalli@gmail.com>
2023-10-01 17:04:34 +0530

    Initial commit

---
+++ myRazorPagesApp/Migrations/20200927073514_AddBookToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace myRazorPagesApp.Migrations
+{
+    public partial class AddBookToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: false),
+                    author = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ myRazorPagesApp/Model/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace myRazorPagesApp.Model
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Book { get; set; }
+     }
+}
